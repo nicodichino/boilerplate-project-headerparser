362d7fecdcdc35528adee209d9f02a6e9be2a68a
nicolasdichino <nicolasdichino@gmail.com>
2024-02-06 20:02:38 -0300

    Solved headers

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  console.log(req.headers);
+  const ipadress = req.headers['x-forwarded-for'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipadress, language, software });
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
